6096334ee16dbb216cd39aa27e5311844ab9af10
Shankie <andrew@properdesign.rs>
2016-04-28 16:54:39 +0100

    Doesn't remove stuff. That's probably good.

--- scripts/proper-entrypoint.sh
+++ scripts/proper-entrypoint.sh
@@ -7,8 +7,8 @@ sed -i -e 's/^exec "$@"/#exec "$@"/g' /entrypoint.sh
 source /entrypoint.sh
 
 # replace parent image wp-content with custom wp-content
-rm -Rf ./wp-content/themes/* && cp -R /usr/src/wordpress/wp-content/themes/* ./wp-content/themes/
-rm -Rf ./wp-content/plugins/* && cp -R /usr/src/wordpress/wp-content/plugins/* ./wp-content/plugins/
+cp -R /usr/src/wordpress/wp-content/themes/* ./wp-content/themes/
+cp -R /usr/src/wordpress/wp-content/plugins/* ./wp-content/plugins/
 chown -R www-data:www-data .
 
 # run additional updates to wp-content and wp-config.php here
